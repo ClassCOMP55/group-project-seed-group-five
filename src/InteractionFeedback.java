bda1ee941c17550efb12cc4734d7657ad556c7e5
Eduardo Montes <e_montes3@u.pacific.edu>
2026-04-07 19:21:35 -0700

    The class for the visual feedback for when the player upgrades a piece

---
+++ src/InteractionFeedback.java
@@ -0,0 +1,26 @@
+public class InteractionFeedback {
+
+    // Highlight tile if merge is valid
+    public void showMergeCue(Tile tile) {
+        // Example: change tile color
+        System.out.println("Valid merge target highlighted");
+    }
+
+    // Remove highlight
+    public void clearCue(Tile tile) {
+        System.out.println("Cue removed");
+    }
+
+    // Snap unit into tile position
+    public void snapUnitToTile(UnitBase unit, Tile tile) {
+        System.out.println("Snapping " + unit.getName() +
+                           " to tile (" + tile.getRow() +
+                           ", " + tile.getCol() + ")");
+    }
+
+    // Check merge hover logic
+    public boolean shouldShowCue(UnitBase dragged, UnitBase target) {
+        return dragged.getName().equals(target.getName()) &&
+               dragged.getStarLevel() == target.getStarLevel();
+    }
+}
