bba10b6e1b77b66de1952c6ac58770a3dabc3f61
Eduardo Montes <e_montes3@u.pacific.edu>
2026-04-17 19:56:44 -0700

    Updating the visual cues for when upgrades happen

--- src/InteractionFeedback.java
+++ src/InteractionFeedback.java
@@ -1,26 +1,94 @@
-public class InteractionFeedback {
+import java.awt.Color;
+import acm.graphics.*;
 
-    // Highlight tile if merge is valid
-    public void showMergeCue(Tile tile) {
-        // Example: change tile colo, but will change this to only either add a star or change color of piece or both
-        System.out.println("Valid merge target highlighted");
+public class InteractionFeedback {
+	private GRect mergeHighlight;
+    private GOval glowRing;
+    private GLabel mergeLabel;
+    private Tile currentTile;
+    
+    public InteractionFeedback() {
+    	mergeHighlight = null;
+    	glowRing = null;
+    	mergeLabel = null;
+    	currentTile = null;
+    	
     }
+
+    public void showMergeHighlight(MainApplication screen, Tile tile) {
+    	if (tile == null) return;
+    	
+    	//Doesn't redraw the highlight in every frame
+    	if (currentTile == tile && mergeHighlight != null) return;
+    	
+        clear(screen);
+
+        double x = tile.getPixelX();
+        double y = tile.getPixelY();
+
+        mergeHighlight = new GRect(x, y, Tile.SIZE, Tile.SIZE);
+        mergeHighlight.setColor(Color.RED);
+        mergeHighlight.setLineWidth(4);
+
+        glowRing = new GOval(x - 4, y - 4, Tile.SIZE + 8, Tile.SIZE + 8);
+        glowRing.setColor(Color.YELLOW);
+        glowRing.setLineWidth(2);
+
+        mergeLabel = new GLabel("MERGE!", x + 4, y - 6);
+        mergeLabel.setColor(Color.YELLOW);
+        mergeLabel.setFont("SansSerif-BOLD-14");
+
+        screen.add(glowRing);
+        screen.add(mergeHighlight);
+        screen.add(mergeLabel);
+        
+        currentTile = tile;
+    }
+    
+    public void clear(MainApplication screen) {
+    	if (mergeHighlight != null) {
+    		screen.remove(mergeHighlight);
+    		mergeHighlight = null;
+    	}
+    	
+    	if (glowRing != null) {
+    		screen.remove(glowRing);
+    		glowRing = null;
+    	}
+    	
+    	if (mergeLabel != null) {
+    		screen.remove(mergeLabel);
+    		mergeLabel = null;
+    	}
+    	
+    	currentTile = null;
+    }
+ 
+
 
     // Remove highlight
     public void clearCue(Tile tile) {
         System.out.println("Cue removed");
     }
 
-    // Snap unit into tile position
-    public void snapUnitToTile(UnitBase unit, Tile tile) {
-        System.out.println("Snapping " + unit.getName() +
-                           " to tile (" + tile.getRow() +
-                           ", " + tile.getCol() + ")");
+
+    
+    public void snapObjectToTile(GObject obj, Tile tile) {
+    	if (obj == null || tile == null) return;
+    	
+        double centeredX = tile.getPixelX() + (Tile.SIZE - obj.getWidth()) / 2;
+        double centeredY = tile.getPixelY() + (Tile.SIZE - obj.getHeight()) / 2;
+        obj.setLocation(centeredX, centeredY);
     }
 
     // Check merge hover logic
-    public boolean shouldShowCue(UnitBase dragged, UnitBase target) {
-        return dragged.getName().equals(target.getName()) &&
-               dragged.getStarLevel() == target.getStarLevel();
+    public boolean shouldShowCue(ChessPiece dragged, ChessPiece target) {
+        return dragged != null &&
+        		target != null &&
+        		dragged.canMergeWith(target);
+    }
+    
+    public boolean isShowing(Tile tile) {
+    	return currentTile == tile && mergeHighlight != null;
     }
 }
